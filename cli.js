64dcdb5bc8bd228ca6e92151f635e17bab409051
Worapan Taengthip <worapan.t@innovestx.co.th>
2025-01-09 18:12:36 +0700

    update script

--- cli.js
+++ cli.js
@@ -4,15 +4,19 @@ const { execSync } = require("child_process");
 const fs = require("fs");
 const path = require("path");
 const inquirer = require("inquirer");
-const { input } = require("@inquirer/prompts");
+const { input, number } = require("@inquirer/prompts");
 
 // Constants
 const TEMPLATE_REPO = "https://github.com/mylovepp/admintools-template.git";
 
 async function main() {
     // Prompt user for input
-    const projectName = await input({ message: "Enter your project name:", validate: (input) => input ? true : "Project name cannot be empty." });
-    
+    const projectName = await input({ message: "Enter your project name:", validate: (input) => input ? true : "Project name cannot be empty." });    
+    // Prompt user for input: default route
+    const defaultRoute = await input({ message: "Enter your default route:", default: "main" });
+    // Prompt user for number: port default 3001, validate port should more than 1000
+    const port = await number({ message: "Enter your port:", default: 3001, validate: (input) => input >= 1000 ? true : "Port should more than 1000." });
+
     // Clone the template
     console.log("Cloning the template repository...");
     execSync(`git clone ${TEMPLATE_REPO} ${projectName}`, { stdio: "inherit" });
@@ -28,6 +32,8 @@ async function main() {
 
     let viteConfig = fs.readFileSync(viteConfigPath, "utf8");
     viteConfig = viteConfig.replace(/{{app-name}}/g, projectName);
+    viteConfig = viteConfig.replace(/{{default-route}}/g, defaultRoute);
+    viteConfig = viteConfig.replace(/{{port}}/g, port);
     fs.writeFileSync(viteConfigPath, viteConfig);
 
     console.log("Project setup complete!");
